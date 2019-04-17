c3415d906fa5c4d03df1d29c17ad00155f917088
Felipe <phil.sousa@gmail.com>
2019-04-17 14:58:56 -0700

    finished testing with new relic

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
 // app.get for Mongo
 app.get('/gallerymongodb/:id', (req, res) => {
   var id = Number(req.params.id);
